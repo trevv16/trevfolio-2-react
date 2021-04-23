b6266a203dd7f357f7287431a4fd1ef1cd6cd6d5
trevv16 <tnjeru16@gmail.com>
2021-04-22 21:42:46 -0400

    fixed nav bottom border design error

--- src/views/projects/ProjectDetailPage.tsx
+++ src/views/projects/ProjectDetailPage.tsx
@@ -75,34 +75,32 @@ export default function ProjectDetailPage() {
   }, []);
 
   const Header = () => (
-    <div className='bg-white'>
-      <div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
-        <div className='text-left'>
-          <div className='mt-1 aspect-w-16 aspect-h-7'>
-            <img className='object-cover shadow-lg rounded-lg' src={project.thumbnail} alt='' />
-          </div>
-          <p className='mt-8 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
-            {project.title}
-          </p>
-          <div className='mt-8 mb-4 space-y-2'>
-            <ul className='flex space-x-5'>
-              <li>
-                <a href={project.github_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
-                  <span className='sr-only'>Github</span>
-                  <HiOutlineCode className='h-6 w-6' />
-                </a>
-              </li>
-              <li>
-                <a href={project.demo_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
-                  <span className='sr-only'>Demo</span>
-                  <HiOutlineEye className='h-6 w-6' />
-                </a>
-              </li>
-            </ul>
-          </div>
-          <p className='mt-2 text-xl text-indigo-600'>{project.published}</p>
-          <p className='mt-8 mx-auto text-xl text-gray-500'>{project.description}</p>
+    <div className='max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8'>
+      <div className='text-left'>
+        <div className='mt-1 aspect-w-16 aspect-h-7'>
+          <img className='object-cover shadow-lg rounded-lg' src={project.thumbnail} alt='' />
         </div>
+        <p className='mt-8 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
+          {project.title}
+        </p>
+        <div className='mt-8 mb-4 space-y-2'>
+          <ul className='flex space-x-5'>
+            <li>
+              <a href={project.github_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>Github</span>
+                <HiOutlineCode className='h-6 w-6' />
+              </a>
+            </li>
+            <li>
+              <a href={project.demo_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>Demo</span>
+                <HiOutlineEye className='h-6 w-6' />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className='mt-2 text-xl text-indigo-600'>{project.published}</p>
+        <p className='mt-8 mx-auto text-xl text-gray-500'>{project.description}</p>
       </div>
     </div>
   );
--- src/views/projects/ProjectsPage.tsx
+++ src/views/projects/ProjectsPage.tsx
@@ -36,7 +36,7 @@ export default function ProjectsPage() {
 
   const ProjectCard = ({ project }: any) => {
     return (
-      <div className='space-y-4'>
+      <div className='my-12 space-y-4'>
         <div className='aspect-w-3 aspect-h-2'>
           <img className='object-cover shadow-lg rounded-lg' src={project.thumbnail} alt='' />
         </div>
@@ -70,26 +70,24 @@ export default function ProjectsPage() {
       <Helmet>
         <title>Projects | Trevor's Portfolio</title>
       </Helmet>
-      <div className='bg-white'>
-        <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24'>
-          <div className='space-y-12'>
-            <div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
-              <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>My Projects</h2>
-              <p className='text-xl text-gray-500'>
-                Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus
-                quis et.
-              </p>
-            </div>
-            <ul className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'>
-              {projects.map((project) => (
-                <a href={`/projects/${project._id}`}>
-                  <li key={project._id}>
-                    <ProjectCard project={project} />
-                  </li>
-                </a>
-              ))}
-            </ul>
+      <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24'>
+        <div className='space-y-12'>
+          <div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
+            <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>My Projects</h2>
+            <p className='text-xl text-gray-500'>
+              Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus
+              quis et.
+            </p>
           </div>
+          <ul className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'>
+            {projects.map((project) => (
+              <a href={`/projects/${project._id}`}>
+                <li key={project._id}>
+                  <ProjectCard project={project} />
+                </li>
+              </a>
+            ))}
+          </ul>
         </div>
       </div>
     </>
