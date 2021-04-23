822dcfdb65703bf97b6c34b03df3b3b09c2997d1
trevv16 <tnjeru16@gmail.com>
2021-04-22 21:42:57 -0400

    added content to skills pages

--- src/views/skills/SkillDetailPage.tsx
+++ src/views/skills/SkillDetailPage.tsx
@@ -1,13 +1,131 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import dayjs from 'dayjs';
+import { HiOutlineEye, HiOutlineCode } from 'react-icons/hi';
+
+type SkillType = {
+  _id: string;
+  category: string;
+  name: string;
+  description: string;
+  thumbnail: string;
+};
 
 export default function SkillDetailPage() {
+  const skill: SkillType = {
+    _id: 'k24l1fsgd323mvb89s',
+    category: 'Front-End',
+    name: 'React',
+    description: 'Skill Description',
+    thumbnail:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+  };
+
+  const projects = [
+    {
+      _id: 'ajk324n2krw6f7ade',
+      title: 'Trevfolio',
+      published: dayjs('2019-01-25').format('MMM D, YYYY'),
+      thumbnail:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      github_url: 'https://github.com/trevva16/trevfolio-2-node',
+      demo_url: 'https://trevornjeru.com'
+    },
+    {
+      _id: 'ajk32adsfarw6f7ade',
+      title: 'Trevfolio',
+      published: dayjs('2019-01-25').format('MMM D, YYYY'),
+      thumbnail:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      github_url: 'https://github.com/trevva16/trevfolio-2-node',
+      demo_url: 'https://trevornjeru.com'
+    },
+    {
+      _id: 'a24n2krwasdfaade',
+      title: 'Trevfolio',
+      published: dayjs('2019-01-25').format('MMM D, YYYY'),
+      thumbnail:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      github_url: 'https://github.com/trevva16/trevfolio-2-node',
+      demo_url: 'https://trevornjeru.com'
+    }
+  ];
+
+  const ProjectCard = ({ project }: any) => {
+    return (
+      <div className='space-y-4'>
+        <div className='aspect-w-3 aspect-h-2'>
+          <img className='object-cover shadow-lg rounded-lg' src={project.thumbnail} alt='' />
+        </div>
+
+        <div className='space-y-2'>
+          <div className='text-lg leading-6 font-medium space-y-1'>
+            <h3 className='text-3xl truncate'>{project.title}</h3>
+            <p className='text-sm text-indigo-600'>{project.published}</p>
+          </div>
+          <ul className='flex space-x-5'>
+            <li>
+              <a href={project.github_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>Github</span>
+                <HiOutlineCode className='h-6 w-6' />
+              </a>
+            </li>
+            <li>
+              <a href={project.demo_url} rel='noopener noreferrer' className='text-gray-400 hover:text-gray-500'>
+                <span className='sr-only'>Demo</span>
+                <HiOutlineEye className='h-6 w-6' />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const ProjectsSection = () => {
+    return (
+      <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-14'>
+        <div className='space-y-12'>
+          <div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
+            <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>Projects</h2>
+            <p className='text-xl text-gray-500'>View the projects I have completed relevant to this skill.</p>
+          </div>
+          <ul className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'>
+            {projects.map((project) => (
+              // <a href={`/projects/${project._id}`}>
+              <li key={project._id}>
+                <ProjectCard project={project} />
+              </li>
+              // {/* </a> */}
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const Header = ({ skill }: any) => (
+    <div className='max-w-7xl mx-auto pt-8 px-4 sm:py-24 sm:px-6 lg:px-8'>
+      <img className='w-52 h-52 mb-8 flex-shrink-0 mx-auto bg-black rounded-full' src={skill.thumbnail} alt='' />
+      <div className='text-center'>
+        <h2 className='text-base font-semibold text-indigo-600 tracking-wide uppercase'>{skill.category}</h2>
+        <p className='mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
+          {skill.name}
+        </p>
+        <p className='max-w-xl mt-5 mx-auto text-xl text-gray-500'>{skill.description}</p>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Helmet>
-        <title>Skill Name | Trevor's Portfolio</title>
+        <title>{`${skill.name} | Trevor's Portfolio`}</title>
       </Helmet>
-      <div></div>
+      <div>
+        <Header skill={skill} />
+        <ProjectsSection />
+      </div>
     </>
   );
 }
--- src/views/skills/SkillsPage.tsx
+++ src/views/skills/SkillsPage.tsx
@@ -1,13 +1,156 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
+type SkillType = {
+  _id: string;
+  category: string;
+  name: string;
+  description: string;
+  thumbnail: string;
+};
+
 export default function SkillsPage() {
+  const [skillCategories, setSkillCategories] = useState<any[]>([]);
+  const skills: SkillType[] = [
+    {
+      _id: 'k24l1fsgd323mvb89s',
+      category: 'Front-End',
+      name: 'React',
+      description: '',
+      thumbnail:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+    },
+    {
+      _id: 'k24l1dfoadfwvb89s',
+      category: 'Back-End',
+      name: 'Node',
+      description: '',
+      thumbnail:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+    },
+    {
+      _id: 'k24l1fsgdsab3bvb89s',
+      category: 'Front-End',
+      name: 'Typescript',
+      description: '',
+      thumbnail:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+    },
+    {
+      _id: 'k24af2m2e2f423bvbafd',
+      category: 'Back-End',
+      name: 'Typescript',
+      description: '',
+      thumbnail:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+    },
+    {
+      _id: 'l2nfada7a5j7l8n42',
+      category: 'Deployment',
+      name: 'AWS',
+      description: '',
+      thumbnail:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+    }
+  ];
+
+  const getSkillCategories = (skills: any) => {
+    let categories: any[] = [];
+
+    skills.forEach((skill: any) => {
+      const currentCategory = skill.category;
+
+      if (!categories.includes(currentCategory)) {
+        categories.push(currentCategory);
+      }
+    });
+
+    return categories;
+  };
+
+  useEffect(() => {
+    const skillList = getSkillCategories(skills);
+
+    setSkillCategories(skillList);
+  }, []);
+
+  const SkillCards = (props: any) => {
+    const data = skills;
+    const skillList = data.filter((skill: any) => {
+      return skill.category === props.categoryName;
+    });
+
+    return (
+      <div className='max-w-7xl mx-auto px-4 text-center sm:px-6 lg:px-8 lg:py-8'>
+        <div className='space-y-8 sm:space-y-12'>
+          <ul className='mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6'>
+            {skillList.map((skill: any) => {
+              return (
+                <a key={skill._id} href={`/skills/${skill._id}`}>
+                  <li>
+                    <div className='space-y-4'>
+                      <img
+                        className='hover:shadow-md mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24'
+                        src={skill.thumbnail}
+                        alt=''
+                      />
+                      <div className='space-y-2'>
+                        <div className='text-xs font-medium lg:text-sm'>
+                          <h3>{skill.name}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </a>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const SkillSection = () => {
+    return (
+      <div>
+        {skillCategories.map((skill: SkillType, i: number) => {
+          return (
+            <div key={`skill-${i}`}>
+              <div className='my-12 relative'>
+                <div className='absolute inset-0 flex items-center' aria-hidden='true'>
+                  <div className='w-full border-t border-gray-300' />
+                </div>
+                <div className='relative flex justify-start'>
+                  <span className='pr-3 bg-white text-lg font-medium text-indigo-600'>{skill}</span>
+                </div>
+              </div>
+              <SkillCards categoryName={skill} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <>
       <Helmet>
         <title>Skills | Trevor's Portfolio</title>
       </Helmet>
-      <div></div>
+      <div className='container mx-auto'>
+        <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24'>
+          <div className='space-y-12'>
+            <div className='space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none'>
+              <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>My Skills</h2>
+              <p className='text-xl text-gray-500'>
+                Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus
+                quis et.
+              </p>
+            </div>
+          </div>
+          <SkillSection />
+        </div>
+      </div>
     </>
   );
 }
