43886c5305c27c63a3372ad64bc74ec365b3e4a0
trevv16 <tnjeru16@gmail.com>
2021-05-04 22:14:57 -0400

    updated seo comp

--- src/components/SeoHelment.tsx
+++ src/components/SeoHelment.tsx
@@ -5,9 +5,9 @@ export default function SeoHelmet(props: any): any {
     <Helmet>
       <title>{props.title}</title>
       <meta name='description' content={props.description} />
-      <meta name={`og:${props.title}`} property='og:Africa Bridges' content={props.description} />
+      <meta name={`og:${props.title}`} property="og:Trevor's Portfolio" content={props.description} />
       <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content='@africabridges' />
+      <meta name='twitter:site' content='@trevva_' />
       <meta name='twitter:title' content={props.title} />
       <meta name='twitter:description' content={props.description} />
       <meta name='twitter:image' content={props.image} />
