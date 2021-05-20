4117feaedd69f7f2d359f92dd9b989769c9f24d8
trevv16 <tnjeru16@gmail.com>
2021-05-20 09:54:36 -0400

    updated resume static content

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import shortid from 'shortid';
 import { BookOpenIcon } from '@heroicons/react/outline';
 import { AiOutlineSafetyCertificate } from 'react-icons/ai';
+import dayjs from 'dayjs';
+import api from '../utils/api';
+import { useFormik } from 'formik';
 
 import { Icon, SkillList } from '../components/index';
+
+type StatusType = {
+  status: string;
+  message: string;
+};
 
 type SkillType = {
   _id: string;
@@ -14,6 +22,54 @@ type SkillType = {
 };
 
 export default function Resume(props: any) {
+  const [statusRes, setStatusRes] = useState<StatusType | null>(null);
+
+  const postData = async (data: any) => {
+    api
+      .post('api/v1/subscribers', data)
+      .then((res) => {
+        const status = res.data.success;
+        if (status === true) {
+          setStatusRes({
+            status: 'success',
+            message: 'Subscribed!'
+          });
+        }
+      })
+      .catch(() => {
+        setStatusRes({
+          status: 'error',
+          message: 'Sorry, we were un able to add you, please try again later.'
+        });
+      });
+  };
+
+  const validate = (values: any) => {
+    const errors: any = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: ''
+    },
+    validate,
+    onSubmit: (values) => {
+      postData(values);
+      formik.resetForm();
+      setTimeout(function () {
+        setStatusRes(null);
+      }, 4000);
+    }
+  });
+
   const skills: SkillType[] = [
     {
       _id: 'k24l1fsgd323mvb89s',
@@ -70,10 +126,13 @@ export default function Resume(props: any) {
           <h1 className='mt-4 font-extrabold text-primary sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl'>
             Full Stack Developer
           </h1>
-          <p className='max-w-2xl mt-8 mx-auto text-xl text-gray-500'>
+          <p className='max-w-2xl my-8 mx-auto text-xl text-gray-500'>
             A computer science student based in Charlotte, NC. Enhancing my skills as a full stack developer with a keen
             interest in design.
           </p>
+          <button className='px-5 py-3 border border-transparent shadow text-base font-bold rounded-md text-white bg-primary hover:bg-primaryLight focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0'>
+            View My Projects
+          </button>
         </div>
       </div>
     </>
@@ -89,6 +148,9 @@ export default function Resume(props: any) {
                 Education
               </p>
             </div>
+            <h1 className='mt-6 text-2xl font-bold text-gray-600'>University of North Carolina at Charlotte</h1>
+            <h2 className='mt-2 text-xl font-medium text-gray-700'>BA in Computer Science</h2>
+            <p className='mt-2 text-xl font-medium text-gray-400'>2016 - In Progress</p>
           </div>
         </div>
       </>
@@ -100,53 +162,48 @@ export default function Resume(props: any) {
       {
         name: 'AWS Cloud Practitioner',
         cert_from: 'Amazon Web Services',
-        date: 'Date',
+        date: '05-10-2021',
         skill: 'AWS',
         skill_icon: 'aws',
-        email: 'janecooper@example.com',
-        cert_link: '#',
-        cert_info_link: '#'
-      },
-      {
-        name: 'AWS Solutions Architect',
-        cert_from: 'Amazon Web Services',
-        date: 'Date',
-        skill: 'AWS',
-        skill_icon: 'aws',
-        email: 'janecooper@example.com',
-        cert_link: '#',
-        cert_info_link: '#'
-      },
-      {
-        name: 'Kubernetes App Developer',
-        cert_from: 'Cloud Native Computing Foundation',
-        date: 'Date',
-        skill: 'Kubernetes',
-        skill_icon: 'kubernetes',
-        email: 'janecooper@example.com',
-        cert_link: '#',
-        cert_info_link: '#'
-      },
-      {
-        name: 'LPIC-1',
-        cert_from: 'Linux Professional Institute',
-        date: 'Date',
-        skill: 'Linux',
-        skill_icon: 'linux',
-        email: 'janecooper@example.com',
-        cert_link: '#',
-        cert_info_link: '#'
-      },
-      {
-        name: 'LPIC-2',
-        cert_from: 'Linux Professional Institute',
-        date: 'Date',
-        skill: 'Linux',
-        skill_icon: 'linux',
-        email: 'janecooper@example.com',
-        cert_link: '#',
-        cert_info_link: '#'
+        cert_link: 'https://www.credly.com/badges/2ed2af89-8b73-40e0-b76a-1bf9f706b7aa/public_url',
+        cert_info_link: 'https://aws.amazon.com/certification/certified-cloud-practitioner/'
       }
+      // {
+      //   name: 'AWS Solutions Architect',
+      //   cert_from: 'Amazon Web Services',
+      //   date: 'Date',
+      //   skill: 'AWS',
+      //   skill_icon: 'aws',
+      //   cert_link: '#',
+      //   cert_info_link: '#'
+      // },
+      // {
+      //   name: 'Kubernetes App Developer',
+      //   cert_from: 'Cloud Native Computing Foundation',
+      //   date: 'Date',
+      //   skill: 'Kubernetes',
+      //   skill_icon: 'kubernetes',
+      //   cert_link: '#',
+      //   cert_info_link: '#'
+      // },
+      // {
+      //   name: 'LPIC-1',
+      //   cert_from: 'Linux Professional Institute',
+      //   date: 'Date',
+      //   skill: 'Linux',
+      //   skill_icon: 'linux',
+      //   cert_link: '#',
+      //   cert_info_link: '#'
+      // },
+      // {
+      //   name: 'LPIC-2',
+      //   cert_from: 'Linux Professional Institute',
+      //   date: 'Date',
+      //   skill: 'Linux',
+      //   skill_icon: 'linux',
+      //   cert_link: '#',
+      //   cert_info_link: '#'
+      // }
     ];
 
     return (
@@ -170,7 +227,7 @@ export default function Resume(props: any) {
                         </span>
                       </div>
                       <p className='mt-1 text-gray-500 text-sm truncate'>{cert.cert_from}</p>
-                      <p className='mt-1 text-gray-500 text-sm truncate'>{cert.date}</p>
+                      <p className='mt-1 text-gray-500 text-sm truncate'>{dayjs(cert.date).format('MMM DD, YYYY')}</p>
                     </div>
                     <Icon size='w-10 h-10' skillName={cert.skill_icon} />
                   </div>
@@ -179,6 +236,8 @@ export default function Resume(props: any) {
                       <div className='w-0 flex-1 flex'>
                         <a
                           href={cert.cert_link}
+                          target='_blank'
+                          rel='noopener noreferrer'
                           className='relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500'
                         >
                           <AiOutlineSafetyCertificate className='w-5 h-5 text-gray-400' aria-hidden='true' />
@@ -188,6 +247,8 @@ export default function Resume(props: any) {
                       <div className='-ml-px w-0 flex-1 flex'>
                         <a
                           href={cert.cert_info_link}
+                          target='_blank'
+                          rel='noopener noreferrer'
                           className='relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500'
                         >
                           <BookOpenIcon className='w-5 h-5 text-gray-400' aria-hidden='true' />
@@ -234,8 +295,35 @@ export default function Resume(props: any) {
           <div className='max-w-7xl mx-auto py-16 sm:py-24 px-6'>
             <div className='text-left'>
               <p className='mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
-                Experience
+                Request My Resume
               </p>
+            </div>
+            <h2 className='mt-2 text-lg font-medium text-gray-700'>
+              Enter an email below and receive a copy of my resume
+            </h2>
+            <div className='mt-8 sm:w-full sm:max-w-md'>
+              <form onSubmit={formik.handleSubmit} className='font-sans sm:flex'>
+                <label htmlFor='emailAddress' className='sr-only'>
+                  Email address
+                </label>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  required
+                  className='w-full border-primary py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white rounded-md'
+                  placeholder='Enter your email'
+                />
+                <button
+                  type='submit'
+                  className='mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-white bg-primary hover:bg-primaryLight focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0'
+                >
+                  Send Resume
+                </button>
+              </form>
             </div>
           </div>
         </div>
@@ -256,7 +344,7 @@ export default function Resume(props: any) {
       </div>
       <div className='font-sans container mx-auto max-w-7xl mt-12'>
         <Skills />
-        <Experience />
+        {/* <Experience /> */}
       </div>
     </>
   );
