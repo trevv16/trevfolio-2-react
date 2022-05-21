e429a28dbef19e1cc2859329f163cf802efee3ac
trevv21 <trevor@litterati.org>
2022-05-21 11:07:46 -0400

    refactored projects page

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,54 +1,7 @@
-import React from 'react';
 import shortid from 'shortid';
-import { HiOutlineEye, HiOutlineCode } from 'react-icons/hi';
-import { Link } from 'react-router-dom';
+import { ProjectCard } from './';
 
 export default function ProjectList(props: any) {
-  const ProjectCard = ({ project }: any) => {
-    return (
-      <div className='my-12 space-y-4'>
-        <Link to={`/projects/${project._id}`}>
-          <div className='aspect-w-3 aspect-h-2'>
-            <img className='object-cover shadow-lg rounded-lg' src={project.thumbnail} alt='' />
-          </div>
-        </Link>
-
-        <div className='space-y-2'>
-          <Link to={`/projects/${project._id}`}>
-            <div className='text-lg leading-6 font-medium space-y-1'>
-              <h3 className='font-sans text-3xl truncate'>{project.title}</h3>
-              <p className='font-sans text-sm text-primary'>{project.published}</p>
-            </div>
-          </Link>
-          <ul className='flex space-x-5'>
-            <li>
-              <a
-                href={project.github_url}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='text-primary-400 hover:text-primary-500'
-              >
-                <span className='sr-only'>Github</span>
-                <HiOutlineCode className='h-6 w-6' /> View Code
-              </a>
-            </li>
-            <li>
-              <a
-                href={project.demo_url}
-                rel='noopener noreferrer'
-                target='_blank'
-                className='text-primary-400 hover:text-primary-500'
-              >
-                <span className='sr-only'>Demo</span>
-                <HiOutlineEye className='h-6 w-6' /> View Demo
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <>
       <div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -10,4 +10,5 @@ export { default as Newsletter } from './Newsletter';
 
 export { default as SkillList } from './SkillList';
 export { default as ProjectList } from './ProjectList';
+export { default as ProjectCard } from './ProjectCard';
 export { default as Resume } from './Resume';
