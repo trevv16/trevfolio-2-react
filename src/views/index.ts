85886813fe79ed290242742f85e6ede122e53659
trevv21 <trevor@litterati.org>
2022-05-21 11:02:13 -0400

    remove unused imports

--- src/views/index.ts
+++ src/views/index.ts
@@ -10,9 +10,3 @@ export { default as SkillDetailPage } from './skills/SkillDetailPage';
 // Error Pages
 export { default as Error404 } from './error/Error404';
 export { default as Error500 } from './error/Error500';
-
-// Auth Pages
-export { default as SignUpPage } from './auth/SignUpPage';
-export { default as SignInPage } from './auth/SignInPage';
-export { default as ForgotPage } from './auth/ForgotPage';
-export { default as ResetPasswordPage } from './auth/ResetPasswordPage';
