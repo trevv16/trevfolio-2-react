edea4d791a0dc736d383da6838616cb89e0f4e78
trevv16 <tnjeru16@gmail.com>
2022-05-21 12:35:57 -0400

    removed unused

--- src/Auth.ts
+++ src/Auth.ts
@@ -1,16 +1,4 @@
 class Auth {
-  static authenticateUser(token: any) {
-    localStorage.setItem('authToken', token);
-  }
-
-  static isUserAuthenticated() {
-    return localStorage.getItem('authToken') !== null;
-  }
-
-  static deauthenticateUser() {
-    localStorage.removeItem('authToken');
-  }
-
   static getToken() {
     return localStorage.getItem('authToken');
   }
